998d7ee3790155bbde7f05f929aa902d565c90d5
Catherine Tran <cathy_tran@CatherinesAir2.attlocal.net>
2022-02-24 23:09:35 -0800

    loops and methods

---
+++ Valid Number and Some Math/src/main/java/com/company/SomeMath.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class SomeMath {
+
+    public static int total5(int a, int b, int c, int d, int e) {
+        int sum = a + b + c + d + e;
+        return sum;
+    }
+
+    public static double average5(int a, int b, int c, int d, int e) {
+        int sum = a + b + c + d + e;
+        double avg = sum / 5;
+        return avg;
+    }
+
+    public static int largest5(double a, double b, double c, double d,
+                               double e) {
+
+        for (int i = 0; i < largest5.length; i++) {
+            if ( > largest.length) {
+                System.out.println
+            }
+        }
+        return largest;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(total5(1,2,3,4,5));
+        System.out.println(average5(1,3,5,7,9));
+        System.out.println(largest5(42.0,35.1,2.3,40.2,15.6));
+    }
+}
---
+++ Valid Number and Some Math/src/main/java/com/company/ValidNumber.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ValidNumber {
+
+    public static void main(String[] args) {
+
+        //call scanner to read user input
+        Scanner myScanner = new Scanner(System.in);
+
+        //prompt user to enter number between 1 and 10 + convert string input to integer
+        System.out.println("Please enter a number between 1 and 10.");
+        String userInput = myScanner.nextLine();
+        int numberEntered = Integer.parseInt(userInput);
+
+//        //while loop until valid number is entered
+//        while (numberEntered > 10 || numberEntered <= 0) {
+//            System.out.println("You must enter a number between 1 and 10. " +
+//                    "Please try again.");
+//        }
+
+//        do {
+//            System.out.println("You must enter a number between 1 and 10. " +
+//                    "Please try again.");
+//        } while (numberEntered > 10 || numberEntered <= 0);
+
+        //print number and exit
+        System.out.println("You have entered " + numberEntered + ".");
+
+    }
+}
